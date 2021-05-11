e8af7b6e090bd9e434577d159256021509ef14a8
Jonathan Rousseau <jonathan.rousseau.wcs@gmail.com>
2021-05-12 00:33:33 +0200

    amelioration parallax

--- src/components/Parallax/Parallax.jsx
+++ src/components/Parallax/Parallax.jsx
@@ -1,73 +1,29 @@
 import React from 'react';
 import ParallaxMousemove from 'react-parallax-mousemove';
-import background from '../../assets/images/background.jpg';
-import bg1 from '../../assets/images/bg1.png';
-import bg2 from '../../assets/images/bg7.png';
-import bg3 from '../../assets/images/bg3.png';
-import bg4 from '../../assets/images/bg4.png';
-import bg5 from '../../assets/images/bg5.png';
-import bg6 from '../../assets/images/bg6.png';
-import bg7 from '../../assets/images/bg7.png';
+
+import fondEtoile from '../../assets/images/fondetoile.jpg';
+import galaxy from '../../assets/images/galaxy.png';
 
 class Parallax extends React.Component {
   render() {
     const style = {
       outter: {
-        background: 'radial-gradient(50% 150%, #6CD7E8 50%, #59C2D3 100%)',
         backgroundPosition: 'center',
         backgroundSize: 'cover',
         width: '100%',
         position: 'relative',
         overflow: 'hidden',
       },
-      inner: {
-        width: 700,
-        height: 500,
-        position: 'absolute',
-        margin: 'auto',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        textAlign: 'center',
-      },
-      header: {
-        fontFamily: 'Open Sans Condensed',
-        textTransform: 'uppercase',
-        color: 'white',
-        textShadow: '0px 0px 2px #43AABA',
-        fontSize: '50px',
-        marginTop: '35px',
-        fontWeight: 600,
-      },
-      paragraph: {
-        fontFamily: 'Roboto',
-        fontSize: '20px',
-        color: 'white',
-        letterSpacing: '0.62px',
-        lineHeight: '30px',
-        fontWeight: 300,
-      },
-      button: {
-        fontFamily: 'Roboto',
-        borderRadius: '100px',
-        background: '#247B8A',
-        textDecoration: 'none',
-        color: 'white',
-        textAlign: 'center',
-        padding: '20px',
-        marginTop: '40px',
-        fontWeight: 100,
-        display: 'block',
-      },
       infoLayerStyle: {
+        background: 'center center/cover',
+        width: '100%',
         position: 'relative',
-        marginTop: '-60px',
+        overflow: 'hidden',
+        height: '100%',
       },
       bgLayerStyle: {
         position: 'absolute',
         height: '100%',
-        transform: 'translate(-15%, 17%)',
       },
     };
 
@@ -85,49 +41,9 @@ class Parallax extends React.Component {
               },
             }}
           >
-            <img src={background} alt='Parallax Layer'></img>
-            <p>Hello</p>
+            <img src={fondEtoile} alt='Parallax Layer'></img>
           </ParallaxMousemove.Layer>
-          <ParallaxMousemove.Layer
-            layerStyle={style.bgLayerStyle}
-            config={{
-              xFactor: 0.15,
-              yFactor: 0,
-              springSettings: {
-                stiffness: 50,
-                damping: 30,
-              },
-            }}
-          >
-            <img src={bg5} alt='Parallax Layer'></img>
-          </ParallaxMousemove.Layer>
-          <ParallaxMousemove.Layer
-            layerStyle={style.bgLayerStyle}
-            config={{
-              xFactor: 0.25,
-              yFactor: 0.025,
-              springSettings: {
-                stiffness: 50,
-                damping: 30,
-              },
-            }}
-          >
-            <img src={bg7} alt='Parallax Layer'></img>
-          </ParallaxMousemove.Layer>
-          <ParallaxMousemove.Layer
-            layerStyle={style.bgLayerStyle}
-            config={{
-              xFactor: 0.4,
-              yFactor: 0.05,
-              springSettings: {
-                stiffness: 50,
-                damping: 30,
-              },
-            }}
-          >
-            <img src={bg6} alt='Parallax Layer'></img>
-          </ParallaxMousemove.Layer>
-          <div style={style.inner}>
+          <div style={style.infoLayerStyle}>
             <ParallaxMousemove.Layer
               layerStyle={style.infoLayerStyle}
               config={{
@@ -139,11 +55,10 @@ class Parallax extends React.Component {
                 },
               }}
             >
-              <img src={bg1} alt='Parallax Layer'></img>
-              <p>Helo</p>
+              <img src={galaxy} alt='Parallax Layer'></img>
             </ParallaxMousemove.Layer>
             <ParallaxMousemove.Layer
-              layerStyle={style.infoLayerStyle}
+              layerStyle={style.bigLayerStyle}
               config={{
                 xFactor: 0.3,
                 yFactor: 0,
@@ -152,22 +67,7 @@ class Parallax extends React.Component {
                   damping: 30,
                 },
               }}
-            >
-              <img src={bg2} alt='Parallax Layer'></img>
-            </ParallaxMousemove.Layer>
-            <ParallaxMousemove.Layer
-              layerStyle={style.infoLayerStyle}
-              config={{
-                xFactor: 0.4,
-                yFactor: 0,
-                springSettings: {
-                  stiffness: 50,
-                  damping: 30,
-                },
-              }}
-            >
-              <img src={bg3} alt='Parallax Layer'></img>
-            </ParallaxMousemove.Layer>
+            ></ParallaxMousemove.Layer>
           </div>
         </ParallaxMousemove>
       </div>
