610abbc69685e5d536d497b36f899a40b256556f
Jonathan Rousseau <jonathan.rousseau.wcs@gmail.com>
2021-05-12 01:54:13 +0200

    amelioration popup

--- src/components/Universe/Popup/Popup.jsx
+++ src/components/Universe/Popup/Popup.jsx
@@ -60,7 +60,9 @@ const Popup = ({ show, click, coordinates, planet, clickVisitPlanet }) => {
       >
         <div className='popupContentContainer'>
           <div>Planet name : </div>
-          <div className='title'>{planet ? planet.name : null}</div>
+          <div className='title'>
+            <h1 className='nomPlanete'>{planet ? planet.name : null}</h1>
+          </div>
           <div className='description'>
             {planet ? planet.description : null}
           </div>
