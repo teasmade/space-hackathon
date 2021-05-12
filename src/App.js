873aed0b994007d42b6835bb4f5a1af6d91068fa
Jonathan Rousseau <jonathan.rousseau.wcs@gmail.com>
2021-05-12 03:56:58 +0200

    test

--- src/App.js
+++ src/App.js
@@ -4,11 +4,35 @@ import Game from './components/Game/Game';
 import './App.scss';
 import PlanetsGrid from './components/Universe/PlanetsGrid';
 import { useEffect, useRef, useState } from 'react';
-import Parallax from './components/Parallax/Parallax.jsx';
+
+import ParallaxMousemove from 'react-parallax-mousemove';
+
+import fondEtoile from './assets/images/fondetoile.jpg';
+import galaxy from './assets/images/galaxy.png';
 
 /* import PlanetsGrid from './components/Universe/PlanetsGrid'; */
 
 function App() {
+  const style = {
+    outter: {
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      width: '100%',
+      position: 'relative',
+      overflow: 'hidden',
+    },
+    infoLayerStyle: {
+      background: 'center center/cover',
+      width: '100%',
+      position: 'relative',
+      overflow: 'hidden',
+      height: '100%',
+    },
+    bgLayerStyle: {
+      position: 'absolute',
+      height: '100%',
+    },
+  };
   const cool = () => {
     console.log('super');
   };
@@ -57,9 +81,66 @@ function App() {
           />
         </div>
       </div>
-      <PlanetsGrid></PlanetsGrid>
-      <Parallax />
-      
+      <div>
+        <ParallaxMousemove containerStyle={style.outter} fullHeight={true}>
+          <ParallaxMousemove.Layer
+            layerStyle={style.bgLayerStyle}
+            config={{
+              xFactor: 0.05,
+              yFactor: 0,
+              springSettings: {
+                stiffness: 50,
+                damping: 30,
+              },
+            }}
+          >
+            <img
+              src={fondEtoile}
+              style={{ width: '120%', transform: 'translateX(-10%)' }}
+              alt='Parallax Layer'
+            ></img>
+          </ParallaxMousemove.Layer>
+          <div style={style.infoLayerStyle}>
+            <ParallaxMousemove.Layer
+              layerStyle={style.infoLayerStyle}
+              config={{
+                xFactor: 0.2,
+                yFactor: 0,
+                springSettings: {
+                  stiffness: 50,
+                  damping: 30,
+                },
+              }}
+            >
+              <img src={galaxy} alt='Parallax Layer'></img>
+            </ParallaxMousemove.Layer>
+            <ParallaxMousemove.Layer
+              layerStyle={style.infoLayerStyle}
+              config={{
+                xFactor: 0.2,
+                yFactor: 0,
+                springSettings: {
+                  stiffness: 50,
+                  damping: 30,
+                },
+              }}
+            >
+              <PlanetsGrid></PlanetsGrid>
+            </ParallaxMousemove.Layer>
+            <ParallaxMousemove.Layer
+              layerStyle={style.bigLayerStyle}
+              config={{
+                xFactor: 0.3,
+                yFactor: 0,
+                springSettings: {
+                  stiffness: 50,
+                  damping: 30,
+                },
+              }}
+            ></ParallaxMousemove.Layer>
+          </div>
+        </ParallaxMousemove>
+      </div>
     </div>
   );
 }
