d983970fae99952a2fee2533988b6bfae1b64341
Thomas Allen <71492408+teasmade@users.noreply.github.com>
2021-05-12 14:20:18 +0200

    final text edits

--- src/components/Universe/Popup/Popup.jsx
+++ src/components/Universe/Popup/Popup.jsx
@@ -2,22 +2,21 @@ import { useEffect, useState } from 'react';
 import './Popup.scss';
 
 const Popup = ({
-  show,
-  click,
-  coordinates,
-  planet,
-  clickVisitPlanet,
-  distance,
-  setFuel,
-  fuel,
-  startGame,
+    show,
+    click,
+    coordinates,
+    planet,
+    clickVisitPlanet,
+    distance,
+    setFuel,
+    fuel,
+    startGame,
 }) => {
-  const [coordinateToDisplay, setCoordinateToDisplay] = useState({
-    left: 0,
-    top: 0,
-  });
-  const [coolButton, setCoolButton] = useState(null);
-
+    const [coordinateToDisplay, setCoordinateToDisplay] = useState({
+        left: 0,
+        top: 0,
+    });
+    const [coolButton, setCoolButton] = useState(null);
 
     const placePopup = () => {
         //place popup to avoid it to overflow
@@ -39,75 +38,63 @@ const Popup = ({
         setCoordinateToDisplay({ left: left, top: top });
     };
 
-  const prepareButton = () => {
-    let buttonToDisplay = null;
-    if (planet && !planet.preVisited && !planet.visited) {
-      buttonToDisplay = (
-        <button
-          className='visitOrInteract'
-          onClick={() => {
-            clickVisitPlanet(planet && planet.id);
-          }}
-        >
-          Visit {planet && planet.name}
-        </button>
-      );
-    } else if (planet && planet.preVisited && !planet.visited) {
-      if (planet.type === 'OIL') {
-        let oil = Math.floor(Math.random() * 400) + 300;
-        setFuel(oil + fuel > 3000 ? 3000 : oil + fuel);
-        buttonToDisplay = (
-          <div>You find a nice stock of oil ! Oil found : {oil}</div>
-        );
-      } else if (planet.type === 'ELON_MUSK') {
-        setTimeout(() => startGame(), 2000);
+    const prepareButton = () => {
+        let buttonToDisplay = null;
+        if (planet && !planet.preVisited && !planet.visited) {
+            buttonToDisplay = (
+                <button
+                    className='visitOrInteract'
+                    onClick={() => {
+                        clickVisitPlanet(planet && planet.id);
+                    }}
+                >
+                    Visit {planet && planet.name}
+                </button>
+            );
+        } else if (planet && planet.preVisited && !planet.visited) {
+            if (planet.type === 'OIL') {
+                let oil = Math.floor(Math.random() * 400) + 300;
+                setFuel(oil + fuel > 3000 ? 3000 : oil + fuel);
+                buttonToDisplay = <div>You find a nice stock of Doge ! Dogecoin found : {oil}</div>;
+            } else if (planet.type === 'ELON_MUSK') {
+                setTimeout(() => startGame(), 2000);
 
-        buttonToDisplay = (
-          <div>You find a clone of Elon Musk !! You must destroy it !</div>
-        );
-      } else buttonToDisplay = <div>Sadly, this planet is empty.</div>;
-    } else if (planet && planet.preVisited && planet.visited) {
-      buttonToDisplay = (
-        <div>You have already visited {planet && planet.name}</div>
-      );
-    }
-    setCoolButton(buttonToDisplay);
-  };
+                buttonToDisplay = <div>You found Elon Musk !! You must stop him !</div>;
+            } else buttonToDisplay = <div>Sadly, this planet is empty.</div>;
+        } else if (planet && planet.preVisited && planet.visited) {
+            buttonToDisplay = <div>You have already visited {planet && planet.name}</div>;
+        }
+        setCoolButton(buttonToDisplay);
+    };
 
-  useEffect(() => {
-    prepareButton();
-    placePopup();
-  }, []);
+    useEffect(() => {
+        prepareButton();
+        placePopup();
+    }, []);
 
-  return (
-    <>
-      <div
-        className={`popupContainer ${show ? 'show' : ''}`}
-        onClick={click}
-      ></div>
-      <div
-        className={`popup ${show ? 'showPop' : ''}`}
-        style={{
-          left: coordinateToDisplay.left,
-          top: coordinateToDisplay.top,
-          position: 'absolute',
-        }}
-      >
-        <div className='popupContentContainer'>
-          <div>Planet name : </div>
-          <div className='title'>
-            <h1 className='nomPlanete'>{planet ? planet.name : null}</h1>
-          </div>
-          <div className='description'>
-            {planet ? planet.description : null}
-          </div>
-          <div>Distance : {distance}</div>
-          {coolButton}
-        </div>
-      </div>
-    </>
-  );
-
+    return (
+        <>
+            <div className={`popupContainer ${show ? 'show' : ''}`} onClick={click}></div>
+            <div
+                className={`popup ${show ? 'showPop' : ''}`}
+                style={{
+                    left: coordinateToDisplay.left,
+                    top: coordinateToDisplay.top,
+                    position: 'absolute',
+                }}
+            >
+                <div className='popupContentContainer'>
+                    <div>Planet name : </div>
+                    <div className='title'>
+                        <h1 className='nomPlanete'>{planet ? planet.name : null}</h1>
+                    </div>
+                    <div className='description'>{planet ? planet.description : null}</div>
+                    <div>Distance : {distance}</div>
+                    {coolButton}
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Popup;
